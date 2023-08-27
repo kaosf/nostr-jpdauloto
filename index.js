a66b90b2d7f9055342ebf5b4e0b5d84a6e3ba993
ka <ka.kaosf@gmail.com>
2023-08-27 22:02:43 +0900

    Extract publish process

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-import {
-  SimplePool,
-  nip19,
-  getEventHash,
-  getPublicKey,
-  getSignature,
-} from "nostr-tools";
+import { SimplePool } from "nostr-tools";
 import "websocket-polyfill";
 import { readFileSync } from "fs";
+import { execSync } from "child_process";
 
 const relays = readFileSync("./relays.txt", "utf-8")
   .split("\n")
   .filter((x) => !x.match(/^#/))
   .filter((x) => !(x === ""));
-const privkey = nip19.decode(readFileSync("./nsec.txt", "utf-8").trim()).data;
-const pubkey = getPublicKey(privkey);
 
 const kojiraPubkey =
   "b3e43e8cc7e6dff23a33d9213a3e912d895b1c3e4250240e0c99dbefe3068b5f";
@@ -49,23 +42,6 @@ sub.on("event", (event) => {
     return;
   }
 
-  const content = `${prediction}`;
-  const ev = {
-    kind: 1,
-    created_at: Math.floor(Date.now() / 1000),
-    tags: [
-      ["e", replyId, "", "root"],
-      ["p", kojiraPubkey],
-    ],
-    content,
-    pubkey,
-  };
-  ev.id = getEventHash(ev);
-  ev.sig = getSignature(ev, privkey);
-
-  try {
-    pool.publish(relays, ev);
-  } catch {
-    console.log("Something went wrong.");
-  }
+  const stdout = execSync(`node publish.js ${replyId} ${prediction}`);
+  console.log(stdout.toString());
 });
